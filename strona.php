6275ba6f01f642a776e2150365cdf7dfdd9ce8ad
Omgaren <90030720+Omgaren@users.noreply.github.com>
2023-11-26 18:19:29 +0100

    członkowie
    Co-Authored-By: Maksymilian <89965215+maksymilianniesluchowski@users.noreply.github.com>
    Co-Authored-By: MaksymilianLammek <89965202+maksymilianlammek@users.noreply.github.com>

---
+++ strona.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Forum internetowe</title>
+</head>
+<script>
+    function myFunction(){
+        var x = document.createElement("BUTTON");
+    }
+</script>
+<body>
+    
+    <div class="gora">
+        <h1>Forum internetowe</h1>
+    </div>
+    <div class="centrum_lewo">
+    </div>
+    <div class="centrum">
+        <button onclik="myFunction()">kolejna zakladka</button>
+    </div>
+    <div class="centrum_prawo">
+    </div>
+    <div class="stopka">
+    </div>
+</body>
+</html>
